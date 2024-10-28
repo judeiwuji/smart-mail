33121f2033c9e74c84d944389064292ef16962e7
Jude Iwuji <105007942+judeiwuji@users.noreply.github.com>
2024-10-28 07:50:44 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'smart-mail';
+  count = signal(0);
+  doubleCount = computed(() => this.count() * 2);
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.count.update((value) => value + 1);
+    });
+  }
 }
