2791c6e7041320c84ca7ab215fb484e7048a0613
Jude Iwuji <105007942+judeiwuji@users.noreply.github.com>
2024-10-31 10:22:51 +0100

    update: manifest

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  INBOX_SDK_APP_ID: 'sdk_smart-mail_f35216d148',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  INBOX_SDK_APP_ID: 'sdk_smart-mail_f35216d148',
+};
